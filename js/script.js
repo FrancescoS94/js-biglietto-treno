3fe3c70d0c4a543339d2470e0f942d8e9c1e7045
FrancescoS <francesco.salerno2294@gmail.com>
2020-07-08 10:37:31 +0200

    End1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
 var km = parseInt (prompt ("Inserisci il numero di km che vuoi percorrere"));
 var eta = parseInt (prompt ("Inserisci la tua età"));
 var prezzobiglietto = km * 0.21;
-var sconto20 = prezzobiglietto * 0.20;
-var sconto40 = prezzobiglietto * 0.40;
+
 var prezzo;
 
 if ( eta < 18 ) {
-    prezzo = prezzobiglietto - sconto20;
+    prezzo = prezzobiglietto - prezzobiglietto * 0.20;
 } else if ( eta > 65 ) {
-    prezzo = prezzobiglietto - sconto40;
+    prezzo = prezzobiglietto - prezzobiglietto * 0.40;
 } else {
     prezzo = prezzobiglietto;
 }
 
-document.getElementById('prezzo').innerHTML = prezzo;
+document.getElementById('prezzo').innerHTML = 'il prezzo che pagherai è di ' + prezzo + '€';
